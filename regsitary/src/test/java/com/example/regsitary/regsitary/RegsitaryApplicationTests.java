75b90f114bc5881a8e8abba44c95b3b6da59cc93
91935 <91935@DESKTOP-PK2AEB5>
2025-07-30 20:06:31 +0530

    Service Repository

---
+++ regsitary/src/test/java/com/example/regsitary/regsitary/RegsitaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.regsitary.regsitary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegsitaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
